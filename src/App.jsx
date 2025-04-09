aa9d6e5d88e3f8f48bfc730ec43a9d614a09de99
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-04-10 00:54:10 +0200

    feat: add column creation dialog with input handling and update Header background color

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { BACKGROUNDS } from "./consts/consts";
 
 function App() {
   const buttonRef = useRef(null);
-
+  const [isCLicked, setIsClicked] = useState(false);
   const [currentBackground, setCurrentBackground] = useState(BACKGROUNDS[0]);
 
   useEffect(() => {
@@ -31,7 +31,21 @@ function App() {
   };
 
   const handleCreateColumn = () => {
-    console.log("Create column");
+    event.preventDefault();
+    setIsClicked(true);
+  };
+
+  const hadleCancellDialog = () => {
+    setIsClicked(false);
+    const dialog = document.querySelector("dialog");
+  };
+
+  const handleSubmitDialog = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const name = formData.get("name");
+    console.log(name);
+    setIsClicked(false);
   };
 
   return (
@@ -48,6 +62,46 @@ function App() {
             Change Background
           </PrimaryActionsButton>
         </div>
+
+        {isCLicked && (
+          <dialog
+            open
+            className="shadow-white-500 mx-auto mt-20 h-50 animate-pulse-fade-in overflow-hidden rounded-xl border-1 border-white bg-neutral-900 opacity-80 shadow-2xl shadow-white/50 md:w-md"
+          >
+            <form
+              method="dialog"
+              onSubmit={handleSubmitDialog}
+              className="flex h-full w-full flex-col items-center justify-center p-4"
+            >
+              <h2 className="text-center text-xl font-medium text-white/90">
+                Put the name of the column ...
+              </h2>
+              <input
+                required
+                autoComplete="off"
+                type="text"
+                className="text-md mx-auto my-4 rounded py-1.5 text-center text-sky-300 placeholder-white/40 outline-2 focus:outline-sky-300"
+                placeholder="Column name"
+                name="name"
+              />
+              <div className="flex justify-end gap-2 pr-10">
+                <button
+                  type="submit"
+                  className="cursor-pointer rounded-md bg-lime-500 px-4 py-2 font-medium text-slate-800 transition-all duration-300 ease-in-out hover:scale-90 hover:bg-slate-800 hover:text-sky-300 hover:outline-2 hover:outline-sky-300"
+                >
+                  Save
+                </button>
+                <button
+                  type="reset"
+                  onClick={hadleCancellDialog}
+                  className="transform cursor-pointer rounded-md bg-yellow-400 px-4 py-2 font-medium text-slate-800 transition-all duration-300 ease-in-out hover:scale-90 hover:bg-slate-800 hover:text-sky-300 hover:outline-2 hover:outline-sky-300"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </dialog>
+        )}
       </Board>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = ({ children }) => {
   return (
-    <header className="backgdrop-blur-xl fixed top-0 right-0 left-0 z-50 flex h-20 items-center justify-center bg-zinc-50 p-4">
+    <header className="fixed top-0 right-0 left-0 z-50 flex h-20 items-center justify-center bg-[var(--color-theme-bg-header)] p-4">
       <h1 className="z-50 flex flex-wrap items-center justify-center gap-2 font-primary text-3xl font-extrabold tracking-wider text-sky-500 antialiased select-none text-shadow-indigo-900 text-shadow-md">
         <span aria-label="A blue book emoji">📘</span>
         {children}
